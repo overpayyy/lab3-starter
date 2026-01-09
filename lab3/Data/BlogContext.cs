d9cd9603cc1d089d50b46b8c1a1a5a7452c192b0
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2026-01-09 21:05:35 +0100

    zadanie 1

---
+++ lab3/Data/BlogContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using lab3.Models.Entities;
+
+namespace lab3.Data
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<BlogArticle> Articles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=blog.db");
+        }
+    }
+}
